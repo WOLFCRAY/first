36cbd541a38655b68502c768f9e0df23ff927608
am11dam2 <alex.pallango.hinojoza@gmail.com>
2021-10-07 08:53:32 +0200

    commit con 2 authores

--- app/src/main/java/com/alemyapp/first/MainActivity.java
+++ app/src/main/java/com/alemyapp/first/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author am11dam2 y alex
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
