b7320703d36e49aa1871eb77dabf7bb9a2db5d38
am11DAM2 <am11DAM2@informatica.iesjc>
2021-10-07 08:44:01 +0200

    Initial commit

---
+++ app/src/main/java/com/alemyapp/first/MainActivity.java
@@ -0,0 +1,17 @@
+package com.alemyapp.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author am11dam2
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
