292ae606f902e63c7272be42e251569daf01ee6b
am11dam2 <alex.pallango.hinojoza@gmail.com>
2021-10-18 12:52:55 +0200

    login y color inverso

--- app/src/main/java/com/alemyapp/first/LoginActivity.java
+++ app/src/main/java/com/alemyapp/first/LoginActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * @author am11dam2 y alex
+ */
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
